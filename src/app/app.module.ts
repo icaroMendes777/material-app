f2e5382238261f803cd147deac2a2a4f9e26b3d2
icaroMendes777 <114268516+icaroMendes777@users.noreply.github.com>
2023-03-15 15:58:03 -0300

    added components
    main project basically done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FirstComponent } from './first/first.component';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCardModule} from '@angular/material/card';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+//import {FormBuilder} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +27,19 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     FirstComponent
   ],
   imports: [
+   // FormBuilder,
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
     MatSlideToggleModule,
     BrowserModule,
     AppRoutingModule,
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSlideToggleModule,  } from '@angular/material/slide-toggle';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormBuilder} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCardModule} from '@angular/material/card';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 
 @Component({
   selector: 'app-first',
@@ -7,5 +21,11 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   styleUrls: ['./first.component.scss']
 })
 export class FirstComponent {
+
+  matDatepicker = '';
+
+  longText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati iste, consequuntur officiis doloremque laborum facilis consequatur repellendus ullam reiciendis ea magnam quam, voluptatum distinctio optio voluptate autem eum delectus! Architecto?';
+
+  constructor(private _formBuilder: FormBuilder) {}
 
 }
